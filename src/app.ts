f0da28355066665295faee59d6262c6dbc33590e
Megas Vituax <megas@vituax.com>
2025-10-13 21:04:08 +0000

    feat: Auto Deploy in api

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "express-async-errors";
 import express from "express";
 import cors from "cors";
 import { logs } from "./middlewares/logs";
+import { autoDeploy } from "./middlewares/autoDeploy";
 import { routes } from "./routes/_index";
 import { notImplemented } from "./middlewares/notImplemented";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -17,6 +18,7 @@ app.use(
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(express.static("public"));
+app.use(autoDeploy);
 app.use(logs);
 app.use(routes);
 app.use(notImplemented);
