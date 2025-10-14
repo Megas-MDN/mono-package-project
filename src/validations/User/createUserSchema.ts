f08c1195f91a98aebcceb498cf35e34f0e4079b5
Megas Melquisedeque <mdn.melqui@gmail.com>
2025-10-14 13:28:25 -0300

    feat: New field on user to test auto deploy

--- src/validations/User/createUserSchema.ts
+++ src/validations/User/createUserSchema.ts
@@ -4,6 +4,7 @@ export const createUserSchema = z.object({
   name: z.string(),
   email: z.string().email(),
   password: z.string().min(6),
+  imageURL: z.string().optional(),
 });
 
 export type TCreateUser = z.infer<typeof createUserSchema>;
