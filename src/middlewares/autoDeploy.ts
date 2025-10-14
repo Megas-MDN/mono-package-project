5c4b27cb098657a701e63feed3b4b1f6cb6d4e40
Megas Melquisedeque <mdn.melqui@gmail.com>
2025-10-14 13:38:32 -0300

    feat: Data now

--- src/middlewares/autoDeploy.ts
+++ src/middlewares/autoDeploy.ts
@@ -2,8 +2,9 @@ import { NextFunction, Request, Response } from "express";
 
 export const autoDeploy = (req: Request, res: Response, next: NextFunction) => {
   const { method, url, body, query } = req;
-  if(!url.endsWith("/api/deploy/github")) return next();
-  if(method !== "POST") return next()
-  console.log("Fazendo o deploy")
-  res.status(200).json({message: `Deploy Done In: ${new Date()}`})
+  if (!url.endsWith("/api/deploy/github")) return next();
+  if (method !== "POST") return next();
+  console.log(`Fazendo Deploy In: ${new Date()}`);
+  res.status(200).json({ message: `Deploy Done In: ${new Date()}` });
+  return;
 };
