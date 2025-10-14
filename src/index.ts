5f5875d91ff1f5f9ffd394390778ae1d48fec9fa
Megas Melquisedeque <mdn.melqui@gmail.com>
2025-10-14 13:42:57 -0300

    feat: Log on start

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ import { app } from "./app";
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log("Server started on port %s 🚀", port);
+  console.log(
+    `Server started on port %s 🚀 \n${new Date()}\n${"=".repeat(20)}`,
+    port,
+  );
 });
