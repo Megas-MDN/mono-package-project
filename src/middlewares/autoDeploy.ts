69277ef94982db9a74b8f47f0cc8e0ff2aaa8376
Megas Melquisedeque <mdn.melqui@gmail.com>
2025-10-14 13:39:56 -0300

    feat: Data agora

--- src/middlewares/autoDeploy.ts
+++ src/middlewares/autoDeploy.ts
@@ -4,7 +4,7 @@ export const autoDeploy = (req: Request, res: Response, next: NextFunction) => {
   const { method, url, body, query } = req;
   if (!url.endsWith("/api/deploy/github")) return next();
   if (method !== "POST") return next();
-  console.log(`Fazendo Deploy In: ${new Date()}`);
+  console.log(`Fazendo Deploy em: ${new Date()}`);
   res.status(200).json({ message: `Deploy Done In: ${new Date()}` });
   return;
 };
