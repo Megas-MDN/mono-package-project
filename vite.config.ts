8cfa5e68b0247fd3e1d75e0fc63df0641a5dac89
Megas Melquisedeque <mdn.melqui@gmail.com>
2025-12-21 12:14:45 -0300

    feat: add PWA support and initialize application entry point with service worker registration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["assets/**/*.svg", "assets/**/*.png", "assets/**/*.ico"],
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        name: "Monozao",
+        short_name: "Monozao",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#121212",
+        theme_color: "#121212",
+        icons: [
+          {
+            src: "assets/fallout.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
   root: "./app",
   build: {
     outDir: "../public",
